ce4afd57bc0b66c5f86727ef8bac0aa082ee1a9b
Dilshod Khajibaev <dilshod.akk2@gmail.com>
2024-02-19 18:15:45 +0300

    added userbook controller

---
+++ Library_WebAPI/Controllers/UserBooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Library_WebAPI.Models;
+
+namespace Library_WebAPI.Controllers
+{
+    public class UserBooksController : ApiController
+    {
+        LibraryEntities entities = new LibraryEntities();
+
+        // GET: UserBooks
+        [HttpGet]
+        public IQueryable<Models.Dtos.UserBooks> GetUserBooks()
+        {
+            var userBooks = from b in entities.UserBooks
+                            select new Models.Dtos.UserBooks()
+                            {
+                                UserBookID = b.UserBookID,
+                                UserID = b.UserID,
+                                BookID = b.BookID,
+                                CheckoutDate = b.CheckoutDate,
+                                ReturnDate = b.ReturnDate
+                            };
+            return userBooks;
+        }
+    }
+}
